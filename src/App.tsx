fd17550e4b66e01dce0bceadebe8c3f6406c2924
Liwen <clw.air@gmail.com>
2018-10-30 11:00:18 +1100

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './App.css';
-
+import { Description } from './components/Description';
+import { Header } from './components/Header';
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -9,11 +10,9 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Header name="REACT" />         
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Description countBy={3} />
       </div>
     );
   }
---
+++ src/components/Header/Header.style.ts
@@ -0,0 +1,12 @@
+import { Box, Flex } from '@rebass/grid';
+import styled from 'styled-components';
+
+export const StyledView = styled(Flex)`
+  background-color: papayawhip;
+  flex: 1;
+  flex-direction: column;
+`;
+
+export const StyledButton = styled(Box)`
+  color: palevioletred;
+`;
