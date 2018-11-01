65cb1dd12be6a5d25fac122ab36e3a6a192bbc54
Liwen <clw.air@gmail.com>
2018-11-01 11:42:00 +1100

    s

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -5,6 +5,4 @@ import { Header } from '.';
 it('renders the correct text when no enthusiasm level is given', () => {
   const header = enzyme.shallow(<Header name='Daniel' />);
   expect(header).toMatchSnapshot();
-  expect(header.find(".greeting").text())
-    .toBe('Hello, Daniel! Welcome to React and TypeScript.')
 });
