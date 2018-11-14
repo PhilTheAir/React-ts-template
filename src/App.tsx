38228e6ce8e6dc4eb98aa58845d6ed9b84417cb1
Liwen <clw.air@gmail.com>
2018-11-14 14:07:48 +1100

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import client from './graphql/apolloClient';
 import Routes from './routes';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Routes />
+      <ApolloProvider client={client}>
+        <Routes />
+      </ApolloProvider>
     );
   }
 }
