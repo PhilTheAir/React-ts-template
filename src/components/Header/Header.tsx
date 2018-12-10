05a0730f0ad5de701423cffdac9e93ec4f8d34bd
Liwen <clw.air@gmail.com>
2018-12-10 13:57:53 +1100

    s

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,14 @@
 import * as React from 'react';
 import { StyledDiv } from './Header.style';
-import { ContextConsumer } from '../../contexts';
 
 interface IProps {
   name?: string;
 }
 
 const Header: React.SFC<IProps> = (props: IProps) => (
-  <ContextConsumer>
-    {
-      contextObj => (
-        <StyledDiv className="greeting">
-          Hello, {props.name || contextObj.name}! Welcome to React and TypeScript.
+  <StyledDiv className="greeting">
+    Hello, {props.name}! Welcome to React and TypeScript.
         </StyledDiv>
-      )
-    }
-  </ContextConsumer>
 );
 
 export default Header;
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import { Header } from '../components/Header';
-import { ContextProvider, stateObj } from '../contexts';
 
 export default class HomeLayout extends React.Component {
   render() {
     return (
-      <ContextProvider value={stateObj}>
-        <Header />
-      </ContextProvider>
+      <Header />
     );
   }
 }
