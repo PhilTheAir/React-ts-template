327e19f1d9539b1a8f6003cb903062fd0a5265af
Liwen <clw.air@gmail.com>
2018-12-29 21:53:14 +1100

    s

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { Mutation } from 'react-apollo';
+import { SIGNUP_MUTATION, LOGIN_MUTATION } from './Login.gql';
 import { AUTH_TOKEN } from '../../auth';
 
 interface IProps {
-  
+  history: any;
 }
 
 interface IState {
@@ -23,14 +25,16 @@ export default class Login extends React.Component<IProps, IState> {
       <div>
         <h4>{login ? 'Login' : 'Sign Up'}</h4>
         <div>
-          {!login && (
-            <input
-              value={name}
-              onChange={e => this.setState({ name: e.target.value })}
-              type="text"
-              placeholder="Your name"
-            />
-          )}
+          {
+            !login && (
+              <input
+                value={name}
+                onChange={e => this.setState({ name: e.target.value })}
+                type="text"
+                placeholder="Your name"
+              />
+            )
+          }
           <input
             value={email}
             onChange={e => this.setState({ email: e.target.value })}
@@ -45,24 +49,38 @@ export default class Login extends React.Component<IProps, IState> {
           />
         </div>
         <div>
-          <div onClick={this.confirm}>
-            {login ? 'login' : 'create account'}
-          </div>
+          <Mutation
+            mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+            variables={{ email, password, name }}
+            onCompleted={(data: any) => this.confirm(data)}
+          >
+            {
+              mutation => (
+                <div onClick={mutation}>
+                  {login ? 'login' : 'create account'}
+                </div>
+              )
+            }
+          </Mutation>
           <div onClick={() => this.setState({ login: !login })}>
-            {login
-              ? 'need to create an account?'
-              : 'already have an account?'}
+            {
+              login
+                ? 'need to create an account?'
+                : 'already have an account?'
+            }
           </div>
         </div>
       </div>
     )
   }
 
-  confirm = async () => {
-    // ... you'll implement this 🔜
+  confirm = async (data: any) => {
+    const { token } = this.state.login ? data.login : data.signup;
+    this.saveUserData(token);
+    this.props.history.push('/');
   }
 
   saveUserData = (token: string) => {
-    localStorage.setItem(AUTH_TOKEN, token)
+    localStorage.setItem(AUTH_TOKEN, token);
   }
 }
--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -1,16 +1,28 @@
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from '../auth';
 
 const uri = 'http://localhost:4000';
 
 const httpLink = createHttpLink({
   uri
-})
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
-})
+});
 
 export default client;
